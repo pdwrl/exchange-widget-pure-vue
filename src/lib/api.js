ea6a7805fe1c72af9b57b4a4b80afc3e1a60a9d3
Pavel Vinogradov <dewaril@gmail.com>
2022-04-30 13:00:12 +0000

    api host changed

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ export const getRates = async (ticker1, ticker2) => {
   const formData = new FormData()
   formData.append('currency_from', ticker1.toLowerCase())
   formData.append('currency_to', ticker2.toLowerCase())
-  const answer = await fetch('https://crypto-verse.info/api/calculator/exchange/calculate/', {
+  const answer = await fetch('https://api.crypto-verse.info/api/calculator/exchange/calculate/', {
     method: 'POST',
     headers: {
       Accept: 'application/json'
